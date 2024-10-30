e4dc1433b353ad9515e3d7443c68c439f5170933
Dominic Omeltschenko <omeltsdf@mail.uc.edu>
2024-10-30 14:46:08 -0400

    Finished most of the looping up to part 7

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Random rnd = new Random();
+        Scanner pipe = new Scanner(System.in);
+        int val = rnd.nextInt(100) + 1;
+        int[] dataPoints;
+        dataPoints = new int[100];
+        double average = 0.00;
+        double sum = 0.00;
+        int valueToFind = 0;
+        int sameAsValue = 0;
+        boolean valueFound = false;
+        int low = 0;
+        int high = 100;
+        for (int i = 0; i < dataPoints.length; i++)
+        {
+            dataPoints[i] = val;
+            val = rnd.nextInt(100) + 1;
         }
+        for (int i = 0; i < dataPoints.length; i++)
+        {
+            System.out.println("Location:"+ i + " " + dataPoints[i]);
+        }
+        for (int i = 0; i < dataPoints.length; i++)
+        {
+            System.out.print(dataPoints[i] + " | ");
+        }
+        for (int i = 0; i < dataPoints.length; i++)
+        {
+            sum += dataPoints[i];
+            average = sum / dataPoints.length;
+
+        }
+        System.out.println();
+        System.out.println("The average of the random Array dataPoints is: " + average + " and the sum is: " + sum);
+
+        valueToFind = SafeInput.getRangedInt(pipe, "Enter an integer" + " [" + low + "-" + high + "]", low, high);
+
+        for (int i = 0; i < dataPoints.length; i++)
+        {
+            if (dataPoints[i] == valueToFind)
+            {
+                sameAsValue += 1;
+            }
+        }
+        System.out.println("The number " + valueToFind + " was found " + sameAsValue + " times in the random array");
+
+        valueToFind = SafeInput.getRangedInt(pipe, "Enter an integer" + " [" + low + "-" + high + "]", low, high);
+
+        for (int i = 0; i < dataPoints.length; i++)
+        {
+            if (dataPoints[i] == valueToFind)
+            {
+                valueFound = true;
+                System.out.println("The value " + valueToFind + " was found at array index " + i + " in the random array");
+                break;
+            }
+        }
+        if (!valueFound)
+        {
+            System.out.println("That value was not found anywhere in the array index");
+        }
+
+
     }
 }
